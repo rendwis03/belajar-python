093459fe3afc19919fd6abd081ae7d6f6b03962f
Rendy_Saputra <rendysaputra4456@gmail.com>
2024-08-23 13:57:16 +0700

    changes if statement

--- if-statement.py
+++ if-statement.py
@@ -1,14 +1,17 @@
-print('Balajar If-Statement')
+# print('Balajar If-Statement')
 
-orang = int (input("Masukkan jumlah orang :"))
-zombie = int (input("Masukkan jumlah zombie :"))
+# orang = int (input("Masukkan jumlah orang :"))
+# zombie = int (input("Masukkan jumlah zombie :"))
 
-if orang < zombie:
-    print ("Kabur aja, dari pada dimakan")
+# if orang < zombie:
+#     print ("Kabur aja, dari pada dimakan")
 
-if orang > zombie:
-    print ("Mari kita bunuh semuaa!!")
+# if orang > zombie:
+#     print ("Mari kita bunuh semuaa!!")
 
-if orang == zombie:
-    print ("Gass perang...")
+# if orang == zombie:
+#     print ("Gass perang...")
 
+
+
+print("hello");
